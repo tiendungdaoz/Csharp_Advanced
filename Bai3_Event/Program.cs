5afee6a8e1c5e7895da113cb65748d4976b911c2
Dao Tien Dung <daotiendung0607@gmai.com>
2023-11-23 13:52:07 +0700

    Add Bai3_Event

--- Bai3_Event/Program.cs
+++ Bai3_Event/Program.cs
@@ -1,14 +1,24 @@
 ﻿namespace Event_trong_Csharp
 {
+    class DuLieuNhap : EventArgs
+    {
+        public int data { set; get; }
+        public DuLieuNhap(int x)
+        {
+            data = x;
+        }
+   
+    }
     class UserInput
     {
         public delegate void SuKienNhapSo(int x);
 
         //Event la delegate, nhung khi khai bao co them tu khoa event
         // => Bien chi co the thuc hien toan tu += hoac -=
-        public event SuKienNhapSo sukiennhapso;
 
-        //public event EventHandler sukiennhapso;   //~ delegate void EventHandler(object? sender, EventArgs e);
+        // public event SuKienNhapSo sukiennhapso;
+
+        public event EventHandler sukiennhapso;   //~ delegate void EventHandler(object? sender, EventArgs e);
 
         public void Input()
         {
@@ -19,7 +29,7 @@
                 int i = Int32.Parse(s);
 
                 //Goi phuong thuc trong delegate tuong duong voi viec phat di su kien
-                sukiennhapso?.Invoke(i);
+                sukiennhapso?.Invoke(this, new DuLieuNhap(i));
             } while (true);
         }
     }
@@ -29,10 +39,18 @@
         {
             input.sukiennhapso += TinhCan;
         }
-        public void TinhCan(int x)
+
+        public void TinhCan(object Sender, EventArgs e)
         {
-            Console.WriteLine($"Can ban 2 cua {x} la: {Math.Sqrt(x)}");
+            DuLieuNhap duLieuNhap = (DuLieuNhap)e;
+            int i = duLieuNhap.data;
+            Console.WriteLine($"Can ban 2 cua {i} la: {Math.Sqrt(i)}");
+
         }
+        //public void TinhCan(int x)
+        //{
+        //    Console.WriteLine($"Can ban 2 cua {x} la: {Math.Sqrt(x)}");
+        //}
     }
 
     class TinhBinhPhuong
@@ -41,10 +59,17 @@
         {
             input.sukiennhapso += BinhPhuong;
         }
-        public void BinhPhuong(int x)
+        public void BinhPhuong(object Sender, EventArgs e)
         {
-            Console.WriteLine($"Binh phuong cua {x} la: {x*x}");
+            DuLieuNhap duLieuNhap = (DuLieuNhap)e;
+            int i = duLieuNhap.data;
+            Console.WriteLine($"Binh phuong cua {i} la: {i*i}");
+
         }
+        //public void BinhPhuong(int x)
+        //{
+        //    Console.WriteLine($"Binh phuong cua {x} la: {x*x}");
+        //}
     }
 
 
@@ -52,6 +77,13 @@
     {
         static void Main(string[] args) 
         {
+            //Su kien Cancel Key Press
+            Console.CancelKeyPress += (object Sender,ConsoleCancelEventArgs e) => 
+            {
+                Console.WriteLine();
+                Console.WriteLine("Thoat ung dung");
+            };
+
             //Publisher
             UserInput user1 = new UserInput();
 
@@ -59,10 +91,16 @@
             TinhCanBac2 tinhCan = new TinhCanBac2();
             TinhBinhPhuong binhPhuong = new TinhBinhPhuong();
 
-            user1.sukiennhapso += (x) => 
+            user1.sukiennhapso += (object Sender, EventArgs e) =>
             {
-                Console.WriteLine("Ban vua nhap so: {0}", x);       //Gan event cho bieu thuc lambda phu hop
+                DuLieuNhap duLieuNhap = (DuLieuNhap)e;
+                int i = duLieuNhap.data;
+                Console.WriteLine("Ban vua nhap so: {0}", i);
             };
+            //user1.sukiennhapso += (x) => 
+            //{
+            //    Console.WriteLine("Ban vua nhap so: {0}", x);       //Gan event cho bieu thuc lambda phu hop
+            //};
 
             tinhCan.Sub(user1);
             binhPhuong.Sub(user1);
