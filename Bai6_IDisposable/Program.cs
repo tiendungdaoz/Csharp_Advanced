3b3fc38eefece0643114943ac870dfd3e89d3819
Dao Tien Dung <daotiendung0607@gmai.com>
2023-11-25 17:11:22 +0700

    Add Bai6_IDisposable

---
+++ Bai6_IDisposable/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Bai6_IDisposable
+{
+    class A: IDisposable
+    {
+        bool resource = true;
+        public void Dispose()   //Phuong thuc giai phong tai nguyen chiem giu-khi doi tuong bi huy
+        {
+            Console.WriteLine("Phuong thuc Dispose tu dong duoc goi khi het Using");
+            resource = false;   //Giai phong tai nguyen
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A obj1 = new A();
+            A obj2 = new A();
+            using(obj1)
+            {
+                Console.WriteLine("Do something ...");
+            }
+        }
+    }
+}
